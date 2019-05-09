641ddefc282cc873cbf54124eb6817dcaaa080fa
hyeyeon2371 <gpdus2371@gmail.com>
2019-05-09 21:34:42 +0900

    Add settingperson layout, MenuButtonView

---
+++ android/ui/src/main/java/com/hyeyeon2371/gaeddal/common/MenuButton.kt
@@ -0,0 +1,37 @@
+package com.hyeyeon2371.gaeddal.common
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.TextView
+import com.hyeyeon2371.gaeddal.R
+
+class MenuButton : ConstraintLayout{
+    constructor(context: Context) : this(context, null)
+    constructor(context:Context, attrs:AttributeSet?) : this(context, attrs , 0){
+        initView(context, attrs)
+    }
+
+    constructor(context: Context, attrs:AttributeSet?, defStyleAttr:Int) : super(context, attrs, defStyleAttr){
+        initView(context, attrs)
+    }
+
+    @SuppressLint("ServiceCast")
+    private fun initView(context: Context, attrs: AttributeSet?) {
+        attrs.let {
+            val li = getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val view = li.inflate(R.layout.view_menubutton, this, false)
+
+            val typedArray = context.obtainStyledAttributes(it, R.styleable.MenuButton, 0, 0)
+            val text = typedArray.getText(typedArray.getResourceId(R.styleable.MenuButton_text, 0))
+
+            addView(view)
+            findViewById<TextView>(R.id.menubutton_text).text =  text.toString()
+            typedArray.recycle()
+        }
+
+    }
+
+}
